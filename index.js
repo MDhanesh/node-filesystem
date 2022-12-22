a907d211f0ac7642126040b808b93db8869a664e
MDhanesh <mgdhanesh98@gmail.com>
2022-12-22 11:38:58 +0530

    j

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+
+//Current TimeStamp
+let currenttime = new Date().toLocaleTimeString();
+//Date-Time
+let datetime = new Date();
+console.log(datetime);
+let timestamp = `${datetime.getDate()}-${datetime.getMonth()}-${datetime.getFullYear()}-${datetime.getHours()}-${datetime.getMinutes()}-${datetime.getSeconds()}`;
+console.log(timestamp);
+
+//Creating folder
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./file/${timestamp}.txt`,
+    `The Current Timestamp
+  is ${currenttime}`,
+    function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    }
+  );
+  res.send({
+    message: "file created successfully",
+  });
+});
+
+//get the data created
+app.get("/get", (req, rest) => {
+  let time = fs.readdirSync(`./file/`);
+  time.forEach((file) => {
+    console.log(file);
+  });
+  res.send(time);
+});
+
+app.listen(3000);
